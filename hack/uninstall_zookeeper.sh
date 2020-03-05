b0c479cda09a02e4ff67ca9e57b0febf2da1a6b3
bilalcaliskan <bilalccaliskan@gmail.com>
2020-03-05 23:20:26 +0300

    final ansible lint fix

--- hack/uninstall_zookeeper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-firewall-cmd --remove-service=zookeeper --permanent
-firewall-cmd --reload
-systemctl stop zookeeper
-systemctl disable zookeeper
-rm -rf /usr/lib/systemd/system/zookeeper.service /etc/firewalld/services/zookeeper.xml
-systemctl daemon-reload
-#rm -rf /opt/apache-zookeeper* /opt/zookeeper*
